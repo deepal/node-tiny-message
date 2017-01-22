3102f6d4b3299c86bff3ae7e0db9a9f10291e506
Deepal Jayasekara <deepal.jayasekara@trycake.com>
2017-01-22 17:37:43 +0530

    add initial project files

---
+++ index.js
@@ -0,0 +1,32 @@
+class Message {
+    static success(message, data, code) {
+        const options = {
+            code: (code !== null && code !== undefined ? code : 0),
+            message: message || '',
+            data: data || {}
+        };
+
+        return {
+            success: true,
+            message: options.message,
+            data: options.data
+        };
+    }
+
+    static error(message, data, code) {
+        const options = {
+            code: (code !== null && code !== undefined ? code : -1),
+            message: message || '',
+            data: data || (new Error('generic error'))
+        };
+
+        return {
+            success: false,
+            code: options.code,
+            message: options.message,
+            data: options.data
+        };
+    }
+}
+
+module.exports = Message;
